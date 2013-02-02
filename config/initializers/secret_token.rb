023d5b58a33f9150b2cae4a0cf2b7e12ec8d93fd
Juan Ignacio Sanz Fuentes <juanignacio.sanz@me.com>
2013-02-02 22:04:37 +0100

    Initial commit
    App layout generated skipping test unit and adding manually rspec as
    the testing framework. Added group scopes for develoment with sqlite,
    testing with rspec and production with postgres.
    Also, rspec installed within rails after bundle update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '79f28e3ceac06eafa3361b3f7cde4407cb03fe58b47748dd8980e62cb6d5be613e9d889ba4640b3b6d43f4463a92eaf78e5ac35538ad1bbb45ba90b788e268ff'
