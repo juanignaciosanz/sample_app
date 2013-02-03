93d259e677fb8a154ac3a9830f5bc5f531574f1b
Juan Ignacio Sanz Fuentes <juanignacio.sanz@me.com>
2013-02-03 01:22:03 +0100

    Contact page added
    As extended functionality for static pages, a test set is added to the
    suite and run until all tests passed. This includes setting a new
    route, change controller and add a template.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,25 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+      
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			page.should have_content('Sample App')
 	    end
+
 		it "should have the title 'Home'" do
       		visit '/static_pages/home'
           page.should have_selector('title',
